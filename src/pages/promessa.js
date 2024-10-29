82b07c90f0810f0b06db987589bb5ca6473587e9
millenyIF <milleny.virissimo@estudante.ifms.edu.br>
2024-10-29 18:21:13 +0000

    aulaGif

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,19 +1,26 @@
 import { Container } from "react-bootstrap";
 import Menu from "./components/navbar";
 import Footer from "./components/footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Promessa(){
     const [divida, setDivida] = useState("")
-    setTimeout(() => {
-        document.title = "Minha Promessa"
-        setDivida("da minha vida")
-    }, 4000);
+    useEffect (() => {
+        setTimeout(() => {
+            document.title = "Minha Promessa"
+            setDivida("da minha vida")
+        }, 4000);
+    })
     return <>
     <Menu />
     <Container>
-        Promessas {(divida==="" ? "<caregando>" : divida)}
+        Promessas {(divida==="" ? <img src="gif.gif" width={40} height={60}/> : divida)}
     </Container>
     <Footer/>
     </>
+}
+function saoLonguinho(texto){
+    setTimeout(() =>{
+        return(texto)
+    },2000)
 }
