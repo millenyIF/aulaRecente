2ef79ba077fb323dae0be3d80eb88fe315918062
millenyIF <milleny.virissimo@estudante.ifms.edu.br>
2024-11-12 18:22:13 +0000

    aulaPages

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -13,6 +13,7 @@ return (
             <Nav.Link href="/lista">Lista</Nav.Link>
             <Nav.Link href="/produtos">Produtos</Nav.Link>
             <Nav.Link href="/promessa">Promessa</Nav.Link>
+            <Nav.Link href="/pages">Pages</Nav.Link>
           </Nav>
         </Navbar.Collapse>
     </Navbar>
